a691f2d05ea2bd91b96cd6f07ab6b5368f1fd23f
Saad Mehmood <saadmehmoodintagleo@gmail.com>
2025-12-16 00:37:50 +0500

    resolve deployment issue

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,18 +1,5 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://portfolio-sandy-two-85.vercel.app";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: `${siteUrl}/sitemap.xml`,
-  };
-}
-import { MetadataRoute } from "next";
-
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
